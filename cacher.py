8ff281ada87760efa70084e11b08803d5e94e999
LittleOrange666 <orangeminecraft123@gmail.com>
2025-12-31 18:53:21 +0800

    auto cache

--- cacher.py
+++ cacher.py
@@ -1,8 +1,12 @@
-from modules import tool
+import time
+from zoneinfo import ZoneInfo
+
 import json
+import datetime
 
 
 def main():
+    from modules import tool
     print("Updating data...")
     tool.update_data()
     print("Caching data...")
@@ -15,5 +19,21 @@ def main():
     print("Caching completed.")
 
 
-if __name__ == '__main__':
+def cal():
+    t = datetime.datetime(2026, 1, 1)
+    print(t)
+    print(t.timestamp())
+    print(t.isoformat())
+    print(datetime.datetime.fromisoformat(t.isoformat()).replace(tzinfo=ZoneInfo("Asia/Taipei")))
+    while datetime.datetime.now() < t:
+        print("now=",datetime.datetime.now())
+        d = t - datetime.datetime.now()
+        print("d=", d)
+        x = d.seconds % 60 + d.microseconds / 1_000_000
+        print("sleeping for", x if x else 60, "seconds")
+        time.sleep(x if x else 60)
     main()
+
+
+if __name__ == '__main__':
+    cal()
